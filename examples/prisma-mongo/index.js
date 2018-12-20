240831a70cb03cbdae4956a645d3e6b5363fa76e
yuyuko <ncysatnaf@gmail.com>
2018-12-20 15:18:46 +0800

    update readme

--- examples/prisma-mongo/index.js
+++ examples/prisma-mongo/index.js
@@ -1,12 +1,11 @@
 // const gqml = require("gqml");
 const gqml = require("../../index");
-const path = require("path");
 
 gqml.use({
   core: {
     start: {
       // plugins:[],
-      modules: ["modules"].map(m => path.join(__dirname, m))
+      modules: ["modules"]
     }
   },
   yoga: {
@@ -15,7 +14,7 @@ gqml.use({
     //   Mutation: {},
     //   Subscription: {}
     // },
-    typeDefs: path.join(__dirname, "./schema.graphql"),
+    typeDefs: "./schema.graphql",
     context: ctx => ctx,
     start: {
       // APOLLO_ENGINE_KEY: "",   @yarn add apollo-engine
