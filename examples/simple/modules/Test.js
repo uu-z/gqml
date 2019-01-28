e6dc5da0f5a67ce8c7a56605648faa40dfa911e1
yuyuko <ncysatnaf@gmail.com>
2019-01-28 20:56:47 +0800

    update example

--- examples/simple/modules/Test.js
+++ examples/simple/modules/Test.js
@@ -1,7 +1,20 @@
+const { p } = require("../utils");
+
 module.exports = {
   yoga: {
+    typeDefs: `${__dirname}/Test.graphql`,
+    // typeDefs: `
+    //   type Query {
+    //     hello(name: String): String!
+    //   }
+    // `,
     resolvers: {
       Query: {
+        Test: {
+          hide: true,
+          shield: p.checkAuth,
+          resolve: (_, { name }) => `Hello ${name || "World"}`
+        },
         hello: (_, { name }) => `Hello ${name || "World"}`
       }
     }
